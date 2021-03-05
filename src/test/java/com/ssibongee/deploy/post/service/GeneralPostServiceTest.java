a7db53a96d2ae372348d15851e43f377ac452d3c
Ahn Siwon <ssibongee@gmail.com>
2021-03-05 10:26:24 +0900

    Update GeneralPostServiceTest.java

--- src/test/java/com/ssibongee/deploy/post/service/GeneralPostServiceTest.java
+++ src/test/java/com/ssibongee/deploy/post/service/GeneralPostServiceTest.java
@@ -37,7 +37,7 @@ class GeneralPostServiceTest {
         when(postRepository.save(any())).thenReturn(post);
         when(post.getId()).thenReturn(1L);
 
-        assertThat(postService.save(postCreateRequest)).isEqualTo(4L);
+        assertThat(postService.save(postCreateRequest)).isEqualTo(6L);
     }
 
     @Test
